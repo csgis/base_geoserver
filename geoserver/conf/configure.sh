a624d6648d5f78c9e7c96f655cabfb6dd5dea68a
Toni <toni.schoenbuchner@csgis.de>
2021-12-04 10:58:21 +0100

    reformat path definitions

--- geoserver/conf/configure.sh
+++ geoserver/conf/configure.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-# SCRIPT TO CUSTOMIZE THE PARENT GEOSERVER -------------------
+# PATH DEFINITIONS ------------------------------------------
+GLOBAL_XML=/var/local/geoserver/global.xml
+USERS_XML=/var/local/geoserver/security/usergroup/default/users.xml 
+CLASSPATH=/usr/local/geoserver/WEB-INF/lib/
+GS_DIR=/var/local/geoserver/geofence/
+GS_PROPERTIES=/var/local/geoserver/geofence/geofence-server.properties
+GEOFENCE_EXTENSION_DIR=/var/local/geoserver-exts/geofence
+
+# FUNCTIONS --------------------------------------------------
 _headline() { 
       printf %0$((40))d\\n | tr 0 \# ;
       echo "$1"
@@ -12,29 +20,19 @@ make_hash(){
     (echo "digest1:" && java -classpath $(find $CLASSPATH -regex ".*jasypt-[0-9]\.[0-9]\.[0-9].*jar") org.jasypt.intf.cli.JasyptStringDigestCLI digest.sh algorithm=SHA-256 saltSizeBytes=16 iterations=100000 input="$NEW_PASSWORD" verbose=0) | tr -d '\n'
 }
 
+
 _headline "GEOSERVER CUSTOM CONFIGURATION START"
-echo "GEOSERVER CUSTOM CONFIGURATION"
-echo "PROXY BASE IS: ${PROXY_BASE}"
-
-
-# PATH DEFINITIONS ------------------------------------------
-GLOBAL_XML=/var/local/geoserver/global.xml
-USERS_XML=/var/local/geoserver/security/usergroup/default/users.xml 
-CLASSPATH=/usr/local/geoserver/WEB-INF/lib/
-GS_DIR=/var/local/geoserver/geofence/
-GS_PROPERTIES=/var/local/geoserver/geofence/geofence-server.properties
-GEOFENCE_EXTENSION_DIR=/var/local/geoserver-exts/geofence
 
 # UPGRADE PROXYBASE URL --------------------------------------
 grep -q proxyBaseUrl $GLOBAL_XML
 if [ $? -eq 0 ]
 then
-	_headline "proxy base definition is present"
+	echo "proxy base definition is present!"
 	echo ${PROXY_BASE}
 	sed -i  -e "s#\(<proxyBaseUrl>\).*\(<\/proxyBaseUrl>\)#<proxyBaseUrl>${PROXY_BASE}<\/proxyBaseUrl>#g"
 	cat $GLOBAL_XML
 else
-	_headline "proxy base definition is not present"
+	echo "proxy base definition is not present!"
 	echo ${PROXY_BASE}
         sed -i "s#<onlineResource>http://geoserver.org</onlineResource>#<onlineResource>http://geoserver.org</onlineResource>\n<proxyBaseUrl>${PROXY_BASE}</proxyBaseUrl>#g" $GLOBAL_XML
 	cat $GLOBAL_XML
